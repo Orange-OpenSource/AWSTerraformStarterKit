be425336f7262346492b1a5bbdb19d5c81daa7c6
Fabien GAUBERT <fabien.gaubert@orange.com>
2023-11-29 17:47:40 +0100

    Add a file to keep the StarterKit Version

--- get-starter-kit.sh
+++ get-starter-kit.sh
@@ -14,7 +14,18 @@
 set -o errexit -o nounset -o pipefail
 
 # Set Starterkit version
-STARTER_KIT_VERSION="${1:-latest}"
+starter_kit_current_version_path=".STARTER_KIT_CURRENT_VERSION"
+touch $starter_kit_current_version_path
+
+# Check if the file exists and is not empty
+if [ -s "$starter_kit_current_version_path" ]; then
+    # Read the content of the file into the variable
+    STARTER_KIT_VERSION=$(<"$starter_kit_current_version_path")
+else
+    # Use the default value if the file is empty or does not exist
+    STARTER_KIT_VERSION="${1:-latest}"
+fi
+
 STARTER_KIT_PROJECT="${2:-Orange-OpenSource/AWSTerraformStarterKit}"
 
 STARTER_KIT_FORMAT="tar"
@@ -25,4 +36,12 @@ if [ "$STARTER_KIT_VERSION" == "latest" ]; then
     STARTER_KIT_LOCATION=$(curl -s ${STARTER_KIT_URL}/releases/latest | jq -r ".${STARTER_KIT_FORMAT}ball_url")
 fi
 
+echo "Download StarterKit from: ${STARTER_KIT_LOCATION}"
+
 curl --fail -L "${STARTER_KIT_LOCATION}" | tar -xz --strip-components 1
+
+STARTER_KIT_VERSION=${STARTER_KIT_LOCATION##*/}
+
+echo "StaterKit Version ${STARTER_KIT_VERSION}"
+
+echo "${STARTER_KIT_VERSION}" > $starter_kit_current_version_path
