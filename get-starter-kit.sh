7cfafa45ee15ef4f5994fbf4652a42f60e2314ec
Fabien GAUBERT <fabien.gaubert@orange.com>
2023-11-29 18:25:05 +0100

    Clean Code

--- get-starter-kit.sh
+++ get-starter-kit.sh
@@ -18,13 +18,13 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 # Set Starterkit version
-starter_kit_current_version_path=".STARTER_KIT_CURRENT_VERSION"
-touch $starter_kit_current_version_path
+STARTER_KIT_CURRENT_VERSION_PATH=".STARTER_KIT_CURRENT_VERSION"
+touch $STARTER_KIT_CURRENT_VERSION_PATH
 
 # Check if the file exists and is not empty
-if [ -s "$starter_kit_current_version_path" ]; then
+if [ -s "$STARTER_KIT_CURRENT_VERSION_PATH" ]; then
     # Read the content of the file into the variable
-    STARTER_KIT_VERSION=$(<"$starter_kit_current_version_path")
+    STARTER_KIT_VERSION=$(<"$STARTER_KIT_CURRENT_VERSION_PATH")
 else
     # Use the default value if the file is empty or does not exist
     STARTER_KIT_VERSION="${1:-latest}"
@@ -37,24 +37,21 @@ STARTER_KIT_URL="https://api.github.com/repos/${STARTER_KIT_PROJECT}"
 STARTER_KIT_LOCATION="${STARTER_KIT_URL}/${STARTER_KIT_FORMAT}ball/${STARTER_KIT_VERSION}"
 
 if [ "$STARTER_KIT_VERSION" == "latest" ]; then
-    STARTER_KIT_LOCATION=$(curl -s ${STARTER_KIT_URL}/releases/latest | jq -r ".${STARTER_KIT_FORMAT}ball_url")
-
+    STARTER_KIT_LOCATION=$(curl -s "${STARTER_KIT_URL}/releases/latest" | jq -r ".${STARTER_KIT_FORMAT}ball_url")
 fi
 
-LAST_PUBLISH_VERSION=$(curl -s ${STARTER_KIT_URL}/releases/latest | jq -r ".tag_name")
+LAST_PUBLISH_VERSION=$(curl -s "${STARTER_KIT_URL}/releases/latest" | jq -r ".tag_name")
 
-echo "Download StarterKit from: ${STARTER_KIT_LOCATION}"
+printf "Download StarterKit from: ${STARTER_KIT_LOCATION}\n"
 
 curl --fail -L --progress-bar "${STARTER_KIT_LOCATION}" | tar -xz --strip-components 1
 
 STARTER_KIT_VERSION=${STARTER_KIT_LOCATION##*/}
 
 if [ "$STARTER_KIT_VERSION" == "$LAST_PUBLISH_VERSION" ]; then
-  printf "Downloaded StaterKit Version ${GREEN}${STARTER_KIT_VERSION}${NC}"
+  printf "Downloaded StaterKit Version ${GREEN}${STARTER_KIT_VERSION}${NC}\n"
 else
-  printf "Downloaded StaterKit Version ${RED}${STARTER_KIT_VERSION}${NC}, the last published version available is ${RED}${LAST_PUBLISH_VERSION}${NC}"
+  printf "Downloaded StaterKit Version ${RED}${STARTER_KIT_VERSION}${NC}, the last published version available is ${RED}${LAST_PUBLISH_VERSION}${NC}\n"
 fi
 
-
-
-echo "${STARTER_KIT_VERSION}" > $starter_kit_current_version_path
+echo "${STARTER_KIT_VERSION}" > "${STARTER_KIT_CURRENT_VERSION_PATH}"
